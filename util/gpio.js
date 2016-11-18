734dcd3c66bf47295843068a668c376bc282b965
Ramzi Mekhail <ramzi@macbook>
2016-11-17 23:24:29 -0800

    Refactored gpio to use config properties

--- util/gpio.js
+++ util/gpio.js
@@ -1,24 +1,22 @@
 var exec = require("child_process").exec;
 var config = require("../config");
 
-var openCmd = "echo 17 > /sys/class/gpio/export";
 var exporturi = "/export";
-var setupCmd = "echo out > /sys/class/gpio/gpio17/direction";
-var onCmd = "echo 1 > /sys/class/gpio/gpio17/value";
-var offCmd = "echo 0 > /sys/class/gpio/gpio17/value";
-var closeCmd = "echo 17 > /sys/class/gpio/unexport"
+var directionuri = "/direction";
+var valueuri = "/value";
+var unexporturi = "/unexport";
 
 var gpio = {
 	opened : false,
 	open : function() {
-		exec("echo " + config.pin + " > " + config.gpiopath + exporturi, (err, stdout, stderr) => {
+		exec(`echo ${config.pin} > ${config.gpiopath}${exporturi}`, (err, stdout, stderr) => {
 			if (err) {
 				console.log("Error opening gpio pin: " + stderr);
 			} else {
 				this.opened = true;
 			}
 		});
-		exec(setupCmd, (err, stdout, stderr) => {
+		exec(`echo out > ${config.gpiopath}/gpio${config.pin}${directionuri}`, (err, stdout, stderr) => {
 			if(err) {
 				console.log("Error initializing gpio pin: " + stderr);
 			}
@@ -26,7 +24,7 @@ var gpio = {
 	},
 	
 	close : function() {
-		exec(closeCmd, (err, stdout, stderr) => {
+		exec(`echo ${config.pin} > ${config.gpiopath}${unexporturi}`, (err, stdout, stderr) => {
 			if(err) {
 				console.log("Error closing gpio pin: " + stderr);
 			}
@@ -36,7 +34,7 @@ var gpio = {
 	on : function() {
 		if(!this.opened)
 			this.open();
-		exec(onCmd, (err, stdout, stderr) => {
+		exec(`echo 1 > ${config.gpiopath}/gpio${config.pin}${valueuri}`, (err, stdout, stderr) => {
 			if(err) {
 				console.log("Error turning on LED: " + stderr);
 			}
@@ -49,7 +47,7 @@ var gpio = {
 	
 	off : function() {
 		 {
-			exec(offCmd, (err, stdout, stderr) => {
+			exec(`echo 0 > ${config.gpiopath}/gpio${config.pin}${valueuri}`, (err, stdout, stderr) => {
 				if(err) {
 					console.log("Error turning off LED: " + stderr);
 				}
