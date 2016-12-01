c40e66952fd244030efca52cc8b06eb1100e322c
Ramzi Mekhail <ramzi@macbook>
2016-12-01 13:27:58 -0800

    Refactors tests to be more concise

--- test/testgpio.js
+++ test/testgpio.js
@@ -4,27 +4,27 @@ var GPIO = require('../util/gpio');
 var config = require('../config.json');
 
 describe('gpio', function() {
-    it('opens a gpio port', function() {
+    beforeEach(function() {
+        config.gpiopath = "/dev/null";
+        config.gpioexport = "/dev/null";
+        config.gpiounexport = "/dev/null";
+        config.gpiodirection = "/dev/null";
+        config.gpioswitchvalue = "/dev/null";
+    })
+
+    describe('.open()', function() {
+        it('should open a gpio port', function() {
         var gpio = new GPIO();
-        testConfig();
         var isReadyFunc = gpio.open();
         expect(isReadyFunc.call(gpio)).to.equal(true);
+        })
     })
-});
-
-describe('gpio', function() {
-    it('turns on a gpio port', function() {
+    
+    describe('.on()', function() {
+        it('should turn on a gpio port', function() {
         var gpio = new GPIO();
-        testConfig();
         var isOnFunc = gpio.on();
         expect(isOnFunc.call(gpio)).to.equal(true);
+        })
     })
 });
-
-var testConfig = function() {
-    config.gpiopath = "/dev/null";
-    config.gpioexport = "/dev/null";
-    config.gpiounexport = "/dev/null";
-    config.gpiodirection = "/dev/null";
-    config.gpioswitchvalue = "/dev/null";
-}
--- test/testruleprovider.js
+++ test/testruleprovider.js
@@ -5,35 +5,37 @@ var Rule = require('../product/rule');
 var config = require('../config.json');
 var Random = require('./random');
 
-describe("test_ruleprovider_addrule", () => {
-    it("Tests adding a rule", () => {
-        var provider = new ruleprovider();
-        //time, duration, rainlevel, repeat
+describe("ruleprovider", function() {
+    describe('.addrule(item)', function() {
+        it("should add a rule", () => {
+            var provider = new ruleprovider();
+            //time, duration, rainlevel, repeat
 
-        var item = new getrule();
+            var item = new getrule();
 
-        provider.addrule(item);
-        var rules = provider.getrules();
-        expect(rules[0].time).to.equal(item.time);
-        expect(rules[0].duration).to.equal(item.duration);
-        expect(rules[0].rainlevel).to.equal(item.rainlevel);
-        expect(rules[0].repeat).to.equal(item.repeat);
-    })
+            provider.addrule(item);
+            var rules = provider.getrules();
+            expect(rules[0].time).to.equal(item.time);
+            expect(rules[0].duration).to.equal(item.duration);
+            expect(rules[0].rainlevel).to.equal(item.rainlevel);
+            expect(rules[0].repeat).to.equal(item.repeat);
+        })
+    });
+
+    describe(".applyrule()", () => {
+        it("should apply a rule", () => {
+            var provider = new ruleprovider();
+            var rule = getrule();
+            rule.time = new Date();
+            provider.rules.push(rule);
+            provider.apply();
+
+            // TODO: write test
+            expect(provider.getgpio().ison()).to.equal(true);
+        })
+    });
 });
 
-describe("test_ruleprovider_applyrule", () => {
-    it("Tests applying a rule", () => {
-        var provider = new ruleprovider();
-        var rule = getrule();
-        rule.time = new Date();
-        provider.rules.push(rule);
-        provider.apply();
-
-        // TODO: write test
-        expect(provider.getgpio().ison()).to.equal(true);
-
-    })
-});
 
 function getrule() {
     var item = new Rule (
